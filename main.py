4641c73b72dbd1107488cbf35e6538e6fb98008c
catcreature <157812154+catcreature@users.noreply.github.com>
2025-01-02 02:08:38 +0530

    pyautogui
    used the automation for the key presses to write a sentence

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import time
+import pyautogui as pyt
 
 number_of_searches = int(input("Enter how many searches you want to perform."))
 
 for i in range(number_of_searches):
     print(f"searching for the {i+1}")
     time.sleep(1)
+    pyt.press("win")
+    time.sleep(1)
+    pyt.write(f"what is the power of 351 to {i}?")
+    time.sleep(1)
+    pyt.press("enter")
+    time.sleep(10)
