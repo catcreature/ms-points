3ef2d0297f692ab0f22bc4ebab1ce259f5bf9345
catcreature <157812154+catcreature@users.noreply.github.com>
2025-01-02 01:49:24 +0530

    added time duration.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import time
+
 number_of_searches = int(input("Enter how many searches you want to perform."))
+
 for i in range(number_of_searches):
     print(f"searching for the {i+1}")
+    time.sleep(1)
