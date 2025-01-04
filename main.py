d8ea54fb38f3e71b8f89a0b6777d0e56c42143dc
catcreature <157812154+catcreature@users.noreply.github.com>
2025-01-04 11:55:15 +0530

    Update main.py
    used the automatic wiki part for the topic searches

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pyautogui as pyt
+import wikipedia as wiki
 
 number_of_searches = int(input("Enter how many searches you want to perform."))
 
+def from_wiki():
+    topic = wiki.random()
+    print(topic)
+    sentences = wiki.summary(topic,sentences=10).split(".")
+    return sentences
+
+sentence = from_wiki()
 for i in range(number_of_searches):
     print(f"searching for the {i+1}")
     pyt.sleep(1)
     pyt.press("win")
     pyt.sleep(1)
-    pyt.write(f"what is the power of 351 to {i}?")
+    pyt.typewrite(sentence[i],0.1)
     pyt.sleep(1)
     pyt.press("enter")
-    pyt.sleep(10)
+    pyt.sleep(5)
