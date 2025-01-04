e234b2504b1bd7a60e5ba1b7bd0d6c0f27ca2e30
catcreature <157812154+catcreature@users.noreply.github.com>
2025-01-04 12:06:12 +0530

    Update main.py
    points calculations based on earned already

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pyautogui as pyt
 import wikipedia as wiki
 
-number_of_searches = int(input("Enter how many searches you want to perform."))
+total_points = int(input("Enter total number of points."))
+points_earned = int(input("Enter the points you have earned."))
+
+def number_of_searches():
+    print(int((total_points - points_earned) / 3))
+    return int((total_points-points_earned)/3)
 
 def from_wiki():
     topic = wiki.random()
@@ -9,13 +14,15 @@ def from_wiki():
     sentences = wiki.summary(topic,sentences=10).split(".")
     return sentences
 
-sentence = from_wiki()
-for i in range(number_of_searches):
-    print(f"searching for the {i+1}")
-    pyt.sleep(1)
-    pyt.press("win")
-    pyt.sleep(1)
-    pyt.typewrite(sentence[i],0.1)
-    pyt.sleep(1)
-    pyt.press("enter")
-    pyt.sleep(5)
+def main():
+    sentence = from_wiki()
+    for i in range(number_of_searches()):
+        print(f"searching for the {i+1}")
+        pyt.sleep(1)
+        pyt.press("win")
+        pyt.sleep(1)
+        pyt.typewrite(sentence[i],0.1)
+        pyt.sleep(1)
+        pyt.press("enter")
+        pyt.sleep(4)
+main()
